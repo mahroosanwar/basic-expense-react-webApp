85de6d8fc2676257774eb7ee261e9162a550647e
MAHROOS ANWAR <mahroos.anwar0901@gmail.com>
2022-09-07 04:25:39 +0530

    Finished

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import './Card.css';
-
 function Card(props) {
     const classes = 'card ' + props.className;
     return <div className={classes}>{props.children}</div>
